415415c64cd0bf61e3b024aa2e094e63d119cdf2
TicGalicia <contacto@ticgalicia.com>
2023-03-23 20:26:25 +0100

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<h2><p>PROYECTO BEBIAN</p></h2>
-
-Bienvenid@ a la página oficial de scripts y recursos para la compilacion (maqueta) BEBIAN de la web www.ticgalicia.com
-
-En esta página se encuentran recursos y scripts aplicables a distribuciones de LINUX Debian (32 y 64 bits)
