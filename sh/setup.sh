926ec2f17cceb391bd53064e8d03230341cbc877
TicGalicia <contacto@ticgalicia.com>
2023-08-09 20:42:23 +0200

    Update setup.sh

--- sh/setup.sh
+++ sh/setup.sh
@@ -46,7 +46,7 @@ echo $1;
 
 ##PARAMETROS DEL SCRIPT:
 stty cols 132 rows 28
-version=3.03.23
+version=3.08.09
 RUTASCRIPT=$(readlink -f $0);
 DIRECTORIO=$(cd "$(dirname "$0")"; pwd -P);
 REPOSITORIO="https://raw.githubusercontent.com/ticgalicia/bebian/master";
