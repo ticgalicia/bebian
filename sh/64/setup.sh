cab201b44437580f420a051e82a1de7b8daeda30
TicGalicia <contacto@ticgalicia.com>
2024-01-22 17:18:11 +0100

    Update setup.sh

--- sh/64/setup.sh
+++ sh/64/setup.sh
@@ -163,7 +163,7 @@ echo "                 Script de preconfiguracion y adaptacion del equipo.";
 echo "     ${cr}Los procesos no se pueden detener y pueden desconfigurar el sistema.";
 echo "               Cierra ahora esta ventana si no sabes como funciona.";
 echo "             En cualquier momento puedes pulsar Ctrl+c para terminar.${cn}";
-
+sleep 1;
 	if [ "$VSIST" = "10.3" ]
 	then
 			echo "${ca}********* Ya existe la ultima version estable del sistema (10.10). ******${cn}";
