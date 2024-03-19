7fd2ce46782725dbd13b8ba36afb032cc7368099
TicGalicia <contacto@ticgalicia.com>
2024-03-19 19:25:51 +0100

    Update setup.sh

--- sh/64/setup.sh
+++ sh/64/setup.sh
@@ -3385,12 +3385,12 @@ mkdir /home/usuario/wine/
 mega-get https://mega.nz/folder/hBgSTKiD#ISyC9foVGxhAayb-wKYmBQ /home/usuario/wine/
 
 echo "[Desktop Entry]" > /home/usuario/Escritorio/CADe_SIMU.desktop;
-echo "Version=3" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
+echo "Version=4" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
 echo "Type=Application" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
 echo "Name=CADe_SIMU" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
-echo "Comment=CADe_SIMU" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
-echo "Exec= wine CADeSIMU.exe" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
-echo "Icon=accessories-text-editor" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
+echo "Comment=" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
+echo "Exec= wine CADe_SIMU.exe" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
+echo "Icon=applications-electronics" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
 echo "Path=/home/usuario/wine/CADe_SIMU/" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
 echo "Terminal=false" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
 echo "StartupNotify=false" >> /home/usuario/Escritorio/CADe_SIMU.desktop;
