12842693978a1749552149fa9e98728ff8938a0c
TicGalicia <contacto@ticgalicia.com>
2024-05-18 11:27:14 +0200

    Update setup.sh

--- sh/64/setup.sh
+++ sh/64/setup.sh
@@ -5467,7 +5467,7 @@ else
 	echo "${cr}Ya puedes ejecutar de nuevo el script.${cn}";
 fi
 clear
-echo "${cv}Puedes forzar la desisntalacion de chrome con -58 y volverlo a instalar... ${cn}";
+echo "${cv}Puedes forzar la desinstalacion de chrome con -58 y volverlo a instalar... ${cn}";
 sleep 2
 # Marcando ultimo update
 echo "$DIA"> setup.log;
