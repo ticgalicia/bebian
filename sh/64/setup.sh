c886b18209a2fb700b9a0e9272f74c357471e187
TicGalicia <contacto@ticgalicia.com>
2024-11-28 11:32:30 +0100

    Update setup.sh

--- sh/64/setup.sh
+++ sh/64/setup.sh
@@ -2222,16 +2222,17 @@ then
 	sleep 1
 	echo "${ca}Decargando...${cn}";
 	sleep 1
-	#wget https://pclinuxos.c3sl.ufpr.br/pclinuxos/addlocale/LO/VMware-Player-Full-17.5.1-23298084.x86_64.bundle
- 	wget https://pclinuxos.c3sl.ufpr.br/pclinuxos/addlocale/LO/VMware_17.5.1_Linux.bundle
-	chmod +x VMware-Player-Full-17.5.1-23298084.x86_64.bundle
+	#wget https://pclinuxos.c3sl.ufpr.br/pclinuxos/addlocale/LO/VMware-Workstation-17.5.1-23298084.x86_64.bundle
+ 	wget https://softwareupdate.vmware.com/cds/vmw-desktop/ws/17.5.1/23298084/linux/core/VMware-Workstation-17.5.1-23298084.x86_64.bundle.tar
+  	tar -xvf VMware-Workstation-17.5.1-23298084.x86_64.bundle.tar
+	chmod +x VMware-Workstation-17.5.1-23298084.x86_64.bundle
  	echo "${ca}Instalando...${cn}";
 	sleep 1
  	apt install -y build-essential linux-headers-$(uname -r)
-	sh ./VMware-Player-Full-17.5.1-23298084.x86_64.bundle
+	sh ./VMware-Workstation-17.5.1-23298084.x86_64.bundle
 	echo "${ca}Borrando...${cn}";
 	sleep 1
-	rm VMware-Player-Full-17.5.1-23298084.x86_64.bundle
+	rm VMware-Workstation-17.5.1-23298084.x86_64.bundle
 fi
 sleep 2
 echo "${ca}Hecho!${cn}";
@@ -2241,15 +2242,7 @@ sleep 1
 opcion42b="1";
 ;;
 
-##Opcion -42b
--42b)
-echo "${ca}Desinstalando player!${cn}";
-vmware-installer -u vmware-player
-sleep 2
-echo "${ca}Hecho!${cn}";
-sleep 1
-opciond42b="0";
-;;
+#vmware-installer -u vmware-player
 
 ##Opcion 43
 43)
